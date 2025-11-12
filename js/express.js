409db80bee80e4a91af84da54ca8baff87980e2f
Dylan Simon <grvdylans@gmail.com>
2025-11-12 11:24:30 -0700

    Finish the "views" section of TOP

--- js/express.js
+++ js/express.js
@@ -3,38 +3,39 @@ import express from "express";
 import authorRouter from "../routes/authorRouter.js";
 import bookRouter from "../routes/bookRouter.js";
 import indexRouter from "../routes/indexRouter.js";
+import npath from "node:path";
+import { fileURLToPath } from "node:url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = npath.dirname(__filename);
+const assetsPath = npath.join(__dirname, "public");
 
 const app = express();
+app.use(express.static(assetsPath));
+app.set("views", npath.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 let path = "./pages/";
 
 app.use("/authors", authorRouter);
 app.use("/books", bookRouter);
+
+const links = [
+    { href: "/", text: "Home" },
+    { href: "about", text: "About" },
+];
+const users = ["Rose", "Cake", "Biff"];
 app.get("/", (req, res) => {
-    fs.readFile(path + "index.html", (err, data) => {
-        if (err) {
-            console.error(err);
-            res.end();
-            return;
-        }
-        res.type("html");
-        res.send(data);
-    });
+    res.render("index", { message: "EJS rocks!", links: links, users: users });
+    console.log(__dirname);
+});
+
+app.get("/about", (req, res) => {
+    res.render("about");
+    console.log(__dirname);
 });
 
 app.get("/contact-me", (req, res) => {
     fs.readFile(path + "contact-me.html", (err, data) => {
-        if (err) {
-            console.error(err);
-            res.end();
-            return;
-        }
-        res.type("html");
-        res.send(data);
-    });
-});
-
-app.get("/about", (req, res) => {
-    fs.readFile(path + "about.html", (err, data) => {
         if (err) {
             console.error(err);
             res.end();
